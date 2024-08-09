aacb28bff96b4b4753bfef8da714f778e2297178
subratap <subratap.eid@gmail.com>
2024-08-09 12:54:14 +0530

    secret key removed

--- codes/user_login_code.php
+++ codes/user_login_code.php
@@ -180,7 +180,7 @@ function resetLoginAttempts($pdo, $username, $ip) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
     $user_id = $username;
-    $recaptcha_secret = 'd';
+    $recaptcha_secret = 'Secret Key Here';
     $recaptcha_response = $_POST['g-recaptcha-response'];
     $ip = getUserIP();
     $latitude = 12345;
